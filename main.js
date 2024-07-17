552f001f1805543227a5f349cf41708eb84b7ba5
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-17 15:59:39 +0200

    feat metiendo las peticiones solo tres y van bien

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './main.scss'
+const CARDS = 3
+
+for (let i = 1; i <= CARDS; i++) {
+  let id = getRandomId(150)
+  searchPokemonById(id)
+}
+
+function getRandomId(max) {
+  return Math.floor(Math.random() * max) + 1
+}
+
+async function searchPokemonById(id) {
+  const res = await fetch('https://pokeapi.co/api/v2/ability/1/')
+  const data = await res.json()
+  console.log(data)
+}
